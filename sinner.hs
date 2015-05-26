5f9cedd1ed064c39df9461754d979fbe1f0767d6
t00n <kelkununtel@hotmail.com>
2015-05-26 18:57:41 +0200

    [add] arithmetic operations on Sinus

--- sinner.hs
+++ sinner.hs
@@ -1,43 +1,57 @@
 import Sound.Pulse.Simple
 
-infixl 6 :+:, :-:
-infixl 7 :*:, :/:
-infixl 8 :**:
+--infixl 6 :+:, :-:
+--infixl 7 :*:, :/:
+--infixl 8 :**:
+--infixl 9 :.:
 
 
-type Period = Float
-type Amplitude = Float
+--type Period = Float
+--type Amplitude = Float
 
-data Func
-    = Var
-    | Lit Float
-    | Func :+: Func
-    | Func :-: Func
-    | Func :*: Func
-    | Func :/: Func
-    | Func :**: Func
-    deriving (Show)
+--data Func
+--    = Var
+--    | Lit Float
+--    | Func :+: Func
+--    | Func :-: Func
+--    | Func :*: Func
+--    | Func :/: Func
+--    | Func :**: Func
+--    | Func :.: Func
+--    deriving (Show)
 
-data Sinus 
-    = Sinus Func
-    deriving (Show)
+--data Sinus 
+--    = Sinus Func
+--    deriving (Show)
 
-evalFunc :: Func -> [Float]
-evalFunc Var = [i / 10 | i <- [0..10]]
-evalFunc (Lit n) = [n | i <- [0..10]]
-evalFunc (a :+: b) = zipWith (+) (evalFunc a) (evalFunc b)
-evalFunc (a :-: b) = zipWith (-) (evalFunc a) (evalFunc b)
-evalFunc (a :*: b) = zipWith (*) (evalFunc a) (evalFunc b)
-evalFunc (a :/: b) = zipWith (/) (evalFunc a) (evalFunc b)
-evalFunc (a :**: b) = zipWith (**) (evalFunc a) (evalFunc b)
+--eval :: Func -> (Float -> Float)
+--eval Var = \x -> x
+--eval (Lit n) = \x -> n
+--eval (a :+: b) = \x -> ((eval a) x) + ((eval b) x)
+--eval (a :-: b) = \x -> ((eval a) x) - ((eval b) x)
+--eval (a :*: b) = \x -> ((eval a) x) * ((eval b) x)
+--eval (a :/: b) = \x -> ((eval a) x) / ((eval b) x)
+--eval (a :**: b) = \x -> ((eval a) x) ** ((eval b) x)
+--eval (a :.: b) = \x -> ((eval a) . (eval b)) x
 
-eval :: Sinus -> [Float]
-eval (Sinus x) = map sin (evalFunc x)
+type Sinus = (Float -> Float)
 
-sinus :: (Float -> Float) -> Float -> (Float -> Float)
+sinus :: Sinus -> Float -> Sinus
 sinus func freq = \t -> sin $ 2 * pi * freq * ((func t) / 44100)
 
-exec :: Simple -> (Float -> Float) -> Float -> IO ()
+sinusAdd :: Sinus -> Sinus -> Sinus
+sinusAdd f1 f2 = \t -> (f1 t) + (f2 t)
+
+sinusSubtract :: Sinus -> Sinus -> Sinus
+sinusSubtract f1 f2 = \t -> (f1 t) - (f2 t)
+
+sinusMultiply :: Sinus -> Sinus -> Sinus
+sinusMultiply f1 f2 = \t -> (f1 t) * (f2 t)
+
+sinusDivide :: Sinus -> Sinus -> Sinus
+sinusDivide f1 f2 = \t -> if (f2 t) == 0 then 0 else (f1 t) / (f2 t)
+
+exec :: Simple -> Sinus -> Float -> IO ()
 exec s f duration = do
     simpleWrite s ([f t | t<-[0..44100*duration]] :: [Float])
 
@@ -45,6 +59,6 @@ main :: IO ()
 main = do
     s<-simpleNew Nothing "Sinner" Play Nothing "This is Sinner" (SampleSpec (F32 LittleEndian) 44100 1) Nothing Nothing
     --exec s (sinus id) 1
-    exec s (sinus (\x -> x*log(x)) 440) 5
+    exec s (sinusDivide (sinus (\x -> x) 440) (sinus(\x -> x) 440)) 2
     simpleDrain s
     simpleFree s
