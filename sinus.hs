d645d4733e773f96d8d3bdf0513f1c1633caa9e5
t00n <kelkununtel@hotmail.com>
2015-05-27 11:04:07 +0200

    [fix] conflict in sinus operators and num operators

--- sinus.hs
+++ sinus.hs
@@ -1,18 +1,18 @@
 module Sinus where
-    
+
 type Sinus = (Float -> Float)
 
 sinus :: Sinus -> Float -> Sinus
-sinus func freq = \t -> sin $ 2 Prelude.* pi Prelude.* freq Prelude.* ((func t) Prelude./ 44100)
+sinus func freq = \t -> sin $ 2 * pi * freq * ((func t) / 44100)
 
-(+) :: Sinus -> Sinus -> Sinus
-f1 + f2 = \t -> (f1 t) Prelude.+ (f2 t)
+(.+) :: Sinus -> Sinus -> Sinus
+f1 .+ f2 = \t -> (f1 t) + (f2 t)
 
-(-) :: Sinus -> Sinus -> Sinus
-f1 - f2 = \t -> (f1 t) Prelude.- (f2 t)
+(.-) :: Sinus -> Sinus -> Sinus
+f1 .- f2 = \t -> (f1 t) - (f2 t)
 
-(*) :: Sinus -> Sinus -> Sinus
-f1 * f2 = \t -> (f1 t) Prelude.* (f2 t)
+(.*) :: Sinus -> Sinus -> Sinus
+f1 .* f2 = \t -> (f1 t) * (f2 t)
 
-(/) :: Sinus -> Sinus -> Sinus
-f1 / f2 = \t -> if (f2 t) == 0 then 0 else (f1 t) Prelude./ (f2 t)
+(./) :: Sinus -> Sinus -> Sinus
+f1 ./ f2 = \t -> if (f2 t) == 0 then 0 else (f1 t) / (f2 t)
