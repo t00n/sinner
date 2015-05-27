001e2b463d907bfaa99689f9bded6c8e68a5d93a
t00n <kelkununtel@hotmail.com>
2015-05-27 18:35:19 +0200

    [fix] note and octave. Cannot go below 20 or above 20k

--- main.hs
+++ main.hs
@@ -11,7 +11,7 @@ main = do
     simple <- simpleNew Nothing "Sinner" Play Nothing "This is Sinner" (SampleSpec (F32 LittleEndian) 44100 1) Nothing Nothing
     let state = PulseState 44100 simple
     flip runStateT state (do 
-        play ((sinus id 220) .* (sinus id 440) .* (sinus id 110) .* (sinus (\x -> x*5) 55)) 1
+        play ((sinus id (note A 2)) .* (sinus id 440) .* (sinus id 110) .* (sinus (\x -> x*5) 55)) 1
         )
     simpleDrain simple
     simpleFree simple
--- sinner.hs
+++ sinner.hs
@@ -1,6 +1,54 @@
 module Sinner where
 
+import Data.List
+import Data.Maybe
+
 type Sinus = (Float -> Float)
+
+data Note
+    = C
+    | CSharp
+    | DFlat
+    | D
+    | DSharp
+    | EFlat
+    | E
+    | ESharp
+    | FFlat
+    | F
+    | FSharp
+    | GFlat
+    | G
+    | GSharp
+    | AFlat
+    | A
+    | ASharp
+    | BFlat
+    | B
+    | CFlat
+    deriving (Show, Ord, Eq)
+
+noteFreq :: (Floating a, Ord a) => Note -> a
+noteFreq C = 261.23
+noteFreq CSharp = 277.18
+noteFreq DFlat = noteFreq CSharp
+noteFreq D = 293.66
+noteFreq DSharp = 311.13
+noteFreq EFlat = noteFreq DSharp
+noteFreq E = 329.63
+noteFreq ESharp = noteFreq F
+noteFreq FFlat = noteFreq E
+noteFreq F = 349.23
+noteFreq FSharp = 369.99
+noteFreq GFlat = noteFreq FSharp
+noteFreq G = 392.00
+noteFreq GSharp = 415.30
+noteFreq AFlat = noteFreq GSharp
+noteFreq A = 440
+noteFreq ASharp = 466.16
+noteFreq BFlat = noteFreq ASharp
+noteFreq B = 493.88
+noteFreq CFlat = noteFreq B
 
 sinus :: Sinus -> Float -> Sinus
 sinus func freq = \t -> sin $ 2 * pi * freq * ((func t) / 44100)
@@ -17,11 +65,12 @@ f1 .* f2 = \t -> (f1 t) * (f2 t)
 (./) :: Sinus -> Sinus -> Sinus
 f1 ./ f2 = \t -> if (f2 t) == 0 then 0 else (f1 t) / (f2 t)
 
-computeFreq :: Floating a => a -> Integer -> a
-computeFreq base octave = base * (2**((fromInteger octave) - 3))
+computeFreq :: (Floating a, Ord a) => a -> Integer -> a
+computeFreq base octave
+    | freq < 20 = computeFreq base (octave+1)
+    | freq > 20000 = computeFreq base (octave-1)
+    | otherwise = freq
+    where freq = base * (2**((fromInteger octave) - 3))
 
-a :: Floating a => Integer -> a
-a = computeFreq 440
-
-b :: Floating a => Integer -> a
-b = computeFreq 493.88
+note :: (Floating a, Ord a) => Note -> Integer -> a
+note n = computeFreq (noteFreq n)
