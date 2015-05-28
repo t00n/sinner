88eeeb4f76e58f5027bf7901980b228efdbdd652
t00n <kelkununtel@hotmail.com>
2015-05-28 03:43:46 +0200

    [add] using modulator in main

--- main.hs
+++ main.hs
@@ -17,10 +17,10 @@ main = do
     simple <- simpleNew Nothing "Sinner" Play Nothing "This is Sinner" (SampleSpec (F32 LittleEndian) 44100 1) Nothing Nothing
     let state = PulseState 44100 simple
     flip runStateT state (do 
-        let sinusoide = (mkSinusoide id (note A 2) 1)
-        let afterMod = amplitudeModulation [AmplitudeModulator (\x -> 0.5) 0 0.25, AmplitudeModulator (\x -> 2) 0.25 0.5] sinusoide
-        --play afterMod
-        liftIO $ plot sinusoide
+        let sinusoide = (mkSinusoide id (note C 2) 0.3) .+ (mkSinusoide id (note C 3) 0.3)
+        let afterMod = amplitudeModulation [AmplitudeModulator (\x -> x) 0 0.2,AmplitudeModulator (\x -> 1.0 - x/5) 0.2 0.3,AmplitudeModulator (\x -> 0.8) 0.2 0.8, AmplitudeModulator (\x -> 0.8 - x*4/5) 0.8 1.0] sinusoide
+        play afterMod
+        --liftIO $ plot sinusoide
         liftIO $ plot afterMod
         )
     simpleDrain simple
