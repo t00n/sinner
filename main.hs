dbd76450d7a77328eb76df99ae8868f0a401fdc9
t00n <kelkununtel@hotmail.com>
2015-06-03 19:07:38 +0200

    [fix] comments in main

--- main.hs
+++ main.hs
@@ -10,18 +10,18 @@ import Sinner
 
 plot :: [Double] -> IO ()
 plot xs = do
-    plotList [] ((zip [0..] xs) :: [(Double, Double)])
+    plotList [] (zip [0..] xs :: [(Double, Double)])
 
 main :: IO ()
 main = do
     simple <- simpleNew Nothing "Sinner" Play Nothing "This is Sinner" (SampleSpec (F32 LittleEndian) 44100 1) Nothing Nothing
     let state = PulseState 44100 simple
     flip runStateT state (do 
-        let start = distortion (0.8 :: Double) $ mkSinusoide (\x -> x) (\t -> (note C 3) - ((note C 3) -(note C 1))*t*t) 0.2
-        let other = (mkSinusoide id (\t -> note C 2) 0.4)
-        let sinusoide = start .+ other
-        let afterMod = amplitudeModulation [AmplitudeModulator (\x -> 1.0 - x/2) 0 0.1,AmplitudeModulator (\x -> 1.0 - x/4) 0.1 0.2,AmplitudeModulator (\x -> 0.8) 0.2 0.5, AmplitudeModulator (\x -> 0.5 - x/2) 0.8 1.0] sinusoide
-        let afterDistortion = afterMod
+        --let start = distortion (0.8 :: Double) $ mkSinusoide (\x -> x) (\t -> (note C 3) - ((note C 3) -(note C 1))*t*t) 0.2
+        --let other = (mkSinusoide id (\t -> note C 2) 0.4)
+        --let sinusoide = start .+ other
+        --let afterMod = amplitudeModulation [AmplitudeModulator (\x -> 1.0 - x/2) 0 0.1,AmplitudeModulator (\x -> 1.0 - x/4) 0.1 0.2,AmplitudeModulator (\x -> 0.8) 0.2 0.5, AmplitudeModulator (\x -> 0.5 - x/2) 0.8 1.0] sinusoide
+        --let afterDistortion = afterMod
         --play sinusoide
         --play afterMod
         --play $ mkSinusoide id (\t -> 0) 0.1
@@ -30,8 +30,8 @@ main = do
         --liftIO $ plot afterMod
         --liftIO $ plot afterDistortion
         noise <- liftIO $ mkWhiteNoise 1.0
-        liftIO $ putStrLn $ show $ length noise
         play noise
+        liftIO $ plot noise
         --liftIO $ plot $ mkWhiteNoise 1
         )
     simpleDrain simple
